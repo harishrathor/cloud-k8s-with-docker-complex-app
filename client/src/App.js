0da35eac3ebc97318df10231a57ac15f6b6e3e82
harishrathor <harishrathor95@gmail.com>
2023-01-27 12:23:24 +0530

    APP-123 UPDATED CODE

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-//import OtherPage from './OtherPage';
-//import Fib from './Fib';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 function App() {
   return (
-    //<Router>
+    <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -19,33 +19,17 @@ function App() {
           >
             Learn React
           </a>
-          <a
-            className="App-link"
-            href="https://google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Google
-          </a>
-          <a
-            className="App-link"
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook
-          </a>
-          {/* <Link to="/">Home</Link> */}
-          {/* <Link to="/otherpage">Other Page</Link> */}
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
           This is body.
-         {/*  <Route exact path="/" component={Fib} /> */}
-          {/* <Route path="/otherpage" component={OtherPage} /> */}
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
 
         </div>
       </div>
-    //</Router>
+    </Router>
   );
 }
 
